35926c914f00926b02fe7d40a65dc1989899b9f9
anngould <anngould@iastate.edu>
2018-04-09 13:17:33 -0500

    finalized workshop

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById("board"); //references id we'll be adjusting in the html
+var ctx = canvas.getContext("2d");
+
+console.log("hey awesome! you made it to the scripts.js file :)");
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)"; //uses transparencies (thats what the extra parameter is for)
+ctx.stroke();
+ctx.closePath();;
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
